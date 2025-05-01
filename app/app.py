634c3bdd79abadb44e81b2bc587fd75262fe5322
anon <mykolassle@gmail.com>
2025-05-01 18:06:01 +0200

    WIP deepsearch

--- app/app.py
+++ app/app.py
@@ -2,18 +2,21 @@ from contextlib import asynccontextmanager
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import httpx
 
+from app.lib.marye import Marye
 from app.lib.youtube import YouTube
 
 
 load_dotenv()
 yt_key = os.getenv("YOUTUBE_API")
+or_key = os.getenv("OPENROUTER_API")
 client = httpx.AsyncClient()
 yt = YouTube(yt_key, client)
+marye = Marye(or_key, client)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -43,8 +46,18 @@ async def search(request: Request, q: str):
 
 @app.get("/watch", response_class=HTMLResponse)
 async def watch(request: Request, v: str):
+    ctx = await yt.get_video_context(v)
+    if not await marye.validate_video(ctx):
+        return RedirectResponse(url="/watch/denied", status_code=302)
     return templates.TemplateResponse(
         request=request,
         name="pages/watch.html",
         context={"video_id": v}
+    )
+
+@app.get("/watch/denied", response_class=HTMLResponse)
+async def watch_denied(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="pages/watch_denied.html"
     )
